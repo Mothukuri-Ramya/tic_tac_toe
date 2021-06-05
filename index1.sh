a9a079488486a8b5647d3693973a65f8ced7a705
ramyamothukuri <83526124+ramyamothukuri@users.noreply.github.com>
2021-06-05 15:01:58 +0530

    Update index1.sh

--- index1.sh
+++ index1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mv 'board.py' 'board'
 sudo chmod +x 'board'
-sudo cp 'board' '/usr/bin/'
+sudo cp 'board' '/usr/bin/tic_tac_toe/'
 alias 'board'='python board'
 cd ..
 sudo rm -r tic_tac_toe
